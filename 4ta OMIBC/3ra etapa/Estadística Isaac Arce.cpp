f5af65d7c86cd6ff2c4e989e59a16068a0aba951
Adolfo Magallanes <magallanesfito@gmail.com>
2016-02-01 22:20:59 -0800

    Create Estadística Isaac Arce.cpp

---
+++ 4ta OMIBC/3ra etapa/Estadística Isaac Arce.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string enun;
+    getline(cin, enun);
+    int contA,contB,contC,contD,contE,contF,contG,contH,contI,contJ,contK,contL,contM,contN,contO,contP,contQ,contR,contS,contT,contU,contV,contW,contX,contY,contZ;
+    contA=0;
+    contB=0;
+    contC=0;
+    contD=0;
+    contE=0;
+    contF=0;
+    contG=0;
+    contH=0;
+    contI=0;
+    contJ=0;
+    contK=0;
+    contL=0;
+    contM=0;
+    contN=0;
+    contO=0;
+    contP=0;
+    contQ=0;
+    contR=0;
+    contS=0;
+    contT=0;
+    contU=0;
+    contV=0;
+    contW=0;
+    contX=0;
+    contY=0;
+    contZ=0;
+    for(int i=0; i<enun.length();i++)
+    {
+        if(enun[i]=='A'||enun[i]=='a')
+        {
+            contA++;
+        }
+        if(enun[i]=='B'||enun[i]=='b')
+        {
+            contB++;
+        }
+        if(enun[i]=='C'||enun[i]=='c')
+        {
+            contC++;
+        }
+        if(enun[i]=='D'||enun[i]=='d')
+        {
+            contD++;
+        }
+        if(enun[i]=='E'||enun[i]=='e')
+        {
+            contE++;
+        }
+        if(enun[i]=='F'||enun[i]=='f')
+        {
+            contF++;
+        }
+        if(enun[i]=='G'||enun[i]=='g')
+        {
+            contG++;
+        }
+        if(enun[i]=='H'||enun[i]=='h')
+        {
+            contH++;
+        }
+        if(enun[i]=='I'||enun[i]=='i')
+        {
+            contI++;
+        }
+        if(enun[i]=='J'||enun[i]=='j')
+        {
+            contJ++;
+        }
+        if(enun[i]=='K'||enun[i]=='k')
+        {
+            contK++;
+        }
+        if(enun[i]=='L'||enun[i]=='l')
+        {
+            contL++;
+        }
+        if(enun[i]=='M'||enun[i]=='m')
+        {
+            contM++;
+        }
+        if(enun[i]=='N'||enun[i]=='n')
+        {
+            contN++;
+        }
+        if(enun[i]=='O'||enun[i]=='o')
+        {
+            contO++;
+        }
+        if(enun[i]=='P'||enun[i]=='p')
+        {
+            contP++;
+        }
+        if(enun[i]=='Q'||enun[i]=='q')
+        {
+            contQ++;
+        }
+        if(enun[i]=='R'||enun[i]=='r')
+        {
+            contR++;
+        }
+        if(enun[i]=='S'||enun[i]=='s')
+        {
+            contS++;
+        }
+        if(enun[i]=='T'||enun[i]=='t')
+        {
+            contT++;
+        }
+        if(enun[i]=='U'||enun[i]=='u')
+        {
+            contU++;
+        }
+        if(enun[i]=='V'||enun[i]=='v')
+        {
+            contV++;
+        }
+        if(enun[i]=='W'||enun[i]=='w')
+        {
+            contW++;
+        }
+        if(enun[i]=='X'||enun[i]=='x')
+        {
+            contX++;
+        }
+        if(enun[i]=='Y'||enun[i]=='y')
+        {
+            contY++;
+        }
+        if(enun[i]=='Z'||enun[i]=='z')
+        {
+            contZ++;
+        }
+    }
+    cout<<contA<<" "<<"A"<<"\n";
+    cout<<contB<<" "<<"B"<<"\n";
+    cout<<contC<<" "<<"C"<<"\n";
+    cout<<contD<<" "<<"D"<<"\n";
+    cout<<contE<<" "<<"E"<<"\n";
+    cout<<contF<<" "<<"F"<<"\n";
+    cout<<contG<<" "<<"G"<<"\n";
+    cout<<contH<<" "<<"H"<<"\n";
+    cout<<contI<<" "<<"I"<<"\n";
+    cout<<contJ<<" "<<"J"<<"\n";
+    cout<<contK<<" "<<"K"<<"\n";
+    cout<<contL<<" "<<"L"<<"\n";
+    cout<<contM<<" "<<"M"<<"\n";
+    cout<<contN<<" "<<"N"<<"\n";
+    cout<<contO<<" "<<"O"<<"\n";
+    cout<<contP<<" "<<"P"<<"\n";
+    cout<<contQ<<" "<<"Q"<<"\n";
+    cout<<contR<<" "<<"R"<<"\n";
+    cout<<contS<<" "<<"S"<<"\n";
+    cout<<contT<<" "<<"T"<<"\n";
+    cout<<contU<<" "<<"U"<<"\n";
+    cout<<contV<<" "<<"V"<<"\n";
+    cout<<contW<<" "<<"W"<<"\n";
+    cout<<contX<<" "<<"X"<<"\n";
+    cout<<contY<<" "<<"Y"<<"\n";
+    cout<<contZ<<" "<<"Z"<<"\n";
+}
