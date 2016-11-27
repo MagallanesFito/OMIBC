7f647ef79bfca6dfc19124d589b734197db57367
Adolfo E. Fragoso Magallanes <magallanesfito@gmail.com>
2016-11-27 13:45:26 -0800

    Examen Preselectivo OMIBC 2016

---
+++ 5ta OMIBC/Caelifera.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+const string numeros = "0123456789";
+void solve(int n,string x){
+    if(n==0){
+        cout << x << endl;
+        return;
+    }
+    else{
+        for(int i=1;i<=n;i++){
+            x = x + numeros[i];
+            soluciones(n-i,x);
+            x  = x.substr(0,x.length()-1);
+        }
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    solve(n,"");
+    return 0;
+}
